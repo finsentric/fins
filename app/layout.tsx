32a72e120c3d006d67c078fbfefaf48d56f52793
Dilip Rajkumar <diliprajkumar@gmail.com>
2025-07-10 23:22:03 +0530

    Added Favicon.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
