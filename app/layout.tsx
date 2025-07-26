da7ee7e462ff9a51ad894fabb093a3472c70f90b
Dilip Rajkumar <diliprajkumar@gmail.com>
2025-07-26 18:47:44 +0530

    Added Google Analytics for Site

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -39,6 +40,19 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" type="image/png" href="/favicon.png" />
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZN5HHQ9S3J"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZN5HHQ9S3J');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
         <ThemeProvider
